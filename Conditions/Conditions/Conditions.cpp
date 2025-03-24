d1a642c81b58635a212a2344459d86c4b5e77584
bekkerVU <bekker.volodymyr.yu@gmail.com>
2025-03-24 13:57:56 +0200

    array lesson

--- Conditions/Conditions/Conditions.cpp
+++ Conditions/Conditions/Conditions.cpp
@@ -9,74 +9,89 @@ int main() {
     SetConsoleOutputCP(1251); // встановлення кодування виводу консолі
     SetConsoleCP(1251); // встановлення кодування вводу консолі
     
-    cout << "Іі Її Єє Ґґ!\n";
+    //cout << "Іі Її Єє Ґґ!\n";
 
-    // Type Conversion - Перетворення типів
+    //// Type Conversion - Перетворення типів
 
-    // Неявне перетворення
-    bool boolean = 10;
+    //// Неявне перетворення
+    //bool boolean = 10;
 
-    int integer1 = 5;
-    float floatNumber = 1.4;
+    //int integer1 = 5;
+    //float floatNumber = 1.4;
 
-    cout << integer1 + floatNumber;
+    //cout << integer1 + floatNumber;
 
-    // Явне перетворення
-    int integer = (int)3.4;
-    
-    double double1 = 4.5;
-    double double2 = 5.6;
+    //// Явне перетворення
+    //int integer = (int)3.4;
+    //
+    //double double1 = 4.5;
+    //double double2 = 5.6;
 
-    cout << (int)(double1 * double2);
+    //cout << (int)(double1 * double2);
 
-    // Звужуюче перетворення
-    float float1 = 5.78;
-    int int1 = float1; // Дробова частина відкидається - втрата інформації
+    //// Звужуюче перетворення
+    //float float1 = 5.78;
+    //int int1 = float1; // Дробова частина відкидається - втрата інформації
 
-    // Розширююче перетворення
-    //int int1 = 5;
-    //double double1 = int1; 
+    //// Розширююче перетворення
+    ////int int1 = 5;
+    ////double double1 = int1; 
 
-    // Перетворення у виразі
-    int i = 27;
-    short s = 2;
-    float  f = 22.3;
-    bool b = false;
+    //// Перетворення у виразі
+    //int i = 27;
+    //short s = 2;
+    //float  f = 22.3;
+    //bool b = false;
 
-    float result = i - f + s * b; // результат типу float
+    //float result = i - f + s * b; // результат типу float
 
-    // Спискова ініціалізація
-    int x = 2.88; // звужуєче перетворення - x зберігає 2
-    // int y = { 3.56 }; // помилка - звужуюче перетворення
+    //// Спискова ініціалізація
+    //int x = 2.88; // звужуєче перетворення - x зберігає 2
+    //// int y = { 3.56 }; // помилка - звужуюче перетворення
 
-    // if
-    int number1, number2;
-    cin >> number1;
-    cin >> number2;
-    if (number1 < number2) {
-        cout << "Перше число менше ніж друге" << endl;
+    //// if
+    //int number1, number2;
+    //cin >> number1;
+    //cin >> number2;
+    //if (number1 < number2) {
+    //    cout << "Перше число менше ніж друге" << endl;
+    //}
+    //else if (number1 > number2) {
+    //    cout << "Перше число більше ніж дурге" << endl;
+    //}
+    //else {
+    //    cout << "Числа рівні" << endl;
+    //}
+
+
+    //// Логічне АБО - ||
+    //int num;
+    //cin >> num;
+
+    //if (num <= 1 || num >= 10) {
+    //    cout << "Число не потрапляє в діапазон від 1 до 10" << endl;
+    //}
+
+    //if ( !(num > 10) ) {
+    //    cout << "Число менше 10" << endl;
+    //}
+
+    //cout << (num % 2 == 0) ? "Число парне" : "Число непарне";
+
+
+    int month;
+    cin >> month;
+
+    switch (month) {
+    case 1: 
+        cout << "Січень" << endl;
+        break;
+    case 2:
+        cout << "Лютий" << endl;
+        break;
+    default: cout << "Невідомій місяць" << endl;
     }
-    else if (number1 > number2) {
-        cout << "Перше число більше ніж дурге" << endl;
-    }
-    else {
-        cout << "Числа рівні" << endl;
-    }
 
-
-    // Логічне АБО - ||
-    int num;
-    cin >> num;
-
-    if (num <= 1 || num >= 10) {
-        cout << "Число не потрапляє в діапазон від 1 до 10" << endl;
-    }
-
-    if ( !(num > 10) ) {
-        cout << "Число менше 10" << endl;
-    }
-
-    cout << (num % 2 == 0) ? "Число парне" : "Число непарне";
 
     // Switch
     int day = 3;
@@ -91,7 +106,7 @@ int main() {
 
     // Switch with enum
     enum PlayerStatus {
-        Online, Offline, Unknown
+        Online = 12, Offline = 1, Unknown = 25
     };
 
     PlayerStatus status = Online;
